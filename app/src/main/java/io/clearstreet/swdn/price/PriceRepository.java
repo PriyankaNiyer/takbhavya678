1f2dd6d2fd4c8d577ccfbc1dad032233638ed7c2
Bhavya Tak <takbhavya678@gmail.com>
2025-09-14 19:56:05 +0530

    Implement core functions and fixed jUnits

--- app/src/main/java/io/clearstreet/swdn/price/PriceRepository.java
+++ app/src/main/java/io/clearstreet/swdn/price/PriceRepository.java
@@ -2,23 +2,28 @@ package io.clearstreet.swdn.price;
 
 import io.clearstreet.swdn.api.PriceApi;
 import io.clearstreet.swdn.model.Price;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
 public class PriceRepository implements PriceApi {
 
-  Map<String, Price> prices = new HashMap<>();
+    private static final Map<String, Price> prices = new HashMap<>();
 
-  @Override
-  public boolean enterPrice(Price price) {
-    prices.put(price.instrumentName(), price);
-    return true;
-  }
+    public static void clearPrices() {
+        prices.clear();
+    }
 
-  public Optional<Double> getPrice(String instrumentName) {
-    return Optional
-        .ofNullable(prices.get(instrumentName))
-        .map(Price::price);
-  }
+    @Override
+    public boolean enterPrice(Price price) {
+        prices.put(price.instrumentName(), price);
+        return true;
+    }
+
+    public Optional<Double> getPrice(String instrumentName) {
+        return Optional
+                .ofNullable(prices.get(instrumentName))
+                .map(Price::price);
+    }
 }
